917f055375a20589f649899a1805301023edfbad
Suzuki Takumi <u1tramarine.takumi@gmail.com>
2021-11-14 01:12:39 +0900

    Refactor, Update design(use Palette API).

--- app/src/main/java/com/u1tramarinet/youtubemusicsharehelper/MainActivity.java
+++ app/src/main/java/com/u1tramarinet/youtubemusicsharehelper/MainActivity.java
@@ -1,215 +1,62 @@
 package com.u1tramarinet.youtubemusicsharehelper;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
 
-import android.content.ContentProvider;
-import android.content.ContentResolver;
-import android.content.ContentUris;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
-import android.widget.Button;
-import android.widget.CompoundButton;
-import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.TextView;
-import android.widget.Toast;
-import android.widget.ToggleButton;
-
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.InputStream;
+import android.text.TextUtils;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView previewText;
-    private TextView previewSuffix;
-    private ImageView previewImage;
-    private Button shareButton;
-    private Button clearTextButton;
-    private Button clearImageButton;
-    private EditText suffixInput;
-    private String text;
-    private Uri imageUri;
+    private MainViewModel viewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Log.d(MainActivity.class.getSimpleName(), "onCreate()");
         setContentView(R.layout.activity_main);
+        viewModel = new ViewModelProvider(this).get(MainViewModel.class);
+        viewModel.shareEvent().observe(this, this::shareText);
+        viewModel.handleIntent(getIntent());
+        viewModel.updateSuffix(obtainSavedSuffix());
+    }
 
-        String suffix = getSuffix();
-        previewText = findViewById(R.id.preview_text);
-        previewSuffix = findViewById(R.id.preview_suffix);
-        previewSuffix.setText(suffix);
-        previewImage = findViewById(R.id.preview_image);
-        clearTextButton = findViewById(R.id.clear_text);
-        clearTextButton.setOnClickListener((v) -> updateText(null));
-        clearImageButton = findViewById(R.id.clear_image);
-        clearImageButton.setOnClickListener((v) -> updateImage(null));
-        shareButton = findViewById(R.id.share);
-        shareButton.setOnClickListener((v) -> share());
-        ToggleButton registerToggle = findViewById(R.id.register_toggle);
-        registerToggle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton compoundButton, boolean checked) {
-                suffixInput.setEnabled(checked);
-                if (!checked) {
-                    updateSuffix(suffixInput.getText().toString());
-                } else {
-                    int length = suffixInput.getText().toString().length();
-                    suffixInput.setSelection(length);
-                    suffixInput.requestFocus();
-                }
-            }
-        });
-        suffixInput = findViewById(R.id.suffix_input);
-        suffixInput.setEnabled(registerToggle.isChecked());
-        suffixInput.setText(suffix);
-
-        updateText(null);
-        updateImage(null);
-
-        handleIntent(getIntent());
+    @Override
+    protected void onPause() {
+        super.onPause();
+        saveSuffix();
     }
 
     @Override
     protected void onNewIntent(Intent intent) {
         super.onNewIntent(intent);
-        Log.d(MainActivity.class.getSimpleName(), "onNewIntent()");
-        handleIntent(intent);
+        viewModel.handleIntent(intent);
     }
 
-    private void handleIntent(Intent intent) {
-        String action = intent.getAction();
-        String type = intent.getType();
-        Log.d(MainActivity.class.getSimpleName(), "handleIntent() action=" + action + ", type=" + type);
-
-        if (!Intent.ACTION_SEND.equals(action)) {
-            return;
-        }
-        if (type == null) {
-            return;
-        }
-
-        if ("text/plain".equals(type)) {
-            handleText(intent);
-        } else if (type.startsWith("image/")) {
-            handleImage(intent);
-        }
-    }
-
-    private void handleText(Intent intent) {
-        String text = parseText(intent.getExtras());
-        Log.d(MainActivity.class.getSimpleName(), "handleText() text=" + text);
-        updateText(text);
-    }
-
-    @Nullable
-    private String parseText(Bundle bundle) {
-        String subject = bundle.getString(Intent.EXTRA_SUBJECT, "");
-        String text = bundle.getString(Intent.EXTRA_TEXT, "");
-
-        String key = "\"";
-        int beginIndex = 1;
-        int endIndex = subject.lastIndexOf(key);
-        Log.d(MainActivity.class.getSimpleName(), "parseText() key=" + key + ", beginIndex=" + beginIndex + ", endIndex=" + endIndex);
-        subject = subject.substring(beginIndex, endIndex);
-
-        if (subject.isEmpty() && text.isEmpty()) return null;
-        if (!subject.isEmpty() && !text.isEmpty()) return subject + "\n" + text;
-        return subject + text;
-    }
-
-    private void handleImage(Intent intent) {
-        Uri imageUri = intent.getParcelableExtra(Intent.EXTRA_STREAM);
-        Log.d(MainActivity.class.getSimpleName(), "handleImage() uri=" + imageUri.getPath());
-        updateImage(imageUri);
-    }
-
-    @Nullable
-    private Bitmap createBitmapFromUri(Uri imageUri) {
-        if (imageUri == null) return null;
-        InputStream stream;
-
-        if (imageUri.getScheme().equals(ContentResolver.SCHEME_CONTENT)) {
-            try {
-                stream = getContentResolver().openInputStream(imageUri);
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
-                Toast.makeText(this, "Failed to get image...", Toast.LENGTH_SHORT).show();
-                return null;
-            }
-        } else {
-            try {
-                stream = new FileInputStream(imageUri.getPath());
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
-                Toast.makeText(this, "Failed to get image...", Toast.LENGTH_SHORT).show();
-                return null;
-            }
-        }
-
-        if (stream == null) {
-            Toast.makeText(this, "Failed to get image...", Toast.LENGTH_SHORT).show();
-            return null;
-        }
-
-        return BitmapFactory.decodeStream(stream);
-    }
-
-    private void share() {
-        if (text == null && imageUri == null) {
-            Toast.makeText(this, "Nothing to share.", Toast.LENGTH_SHORT).show();
-            return;
-        }
-
+    private void shareText(@NonNull Bundle extras) {
         Intent intent = new Intent();
         intent.setAction(Intent.ACTION_SEND);
-
-        if (text != null) {
-            String suffix = getSuffix();
-            if (!suffix.isEmpty()) {
-                suffix = "\n" + suffix;
-            }
-            intent.putExtra(Intent.EXTRA_TEXT, text + suffix);
-        }
-        if (imageUri != null) {
-            intent.putExtra(Intent.EXTRA_STREAM, imageUri);
-        }
         intent.setType("text/plain");
+        intent.putExtras(extras);
         startActivity(intent);
+        saveSuffix();
     }
 
-    private void updateText(String text) {
-        this.text = text;
-        previewText.setText(text);
-        clearTextButton.setEnabled((text != null));
-        shareButton.setEnabled(clearTextButton.isEnabled() || clearImageButton.isEnabled());
+    private void saveSuffix() {
+        String suffix = viewModel.previewSuffix().getValue();
+        String current = obtainSavedSuffix();
+        if (!TextUtils.equals(current, suffix)) {
+            SharedPreferences preferences = getSharedPreferences();
+            SharedPreferences.Editor editor = preferences.edit();
+            editor.putString(getString(R.string.key_suffix), suffix);
+            editor.apply();
+        }
     }
 
-    private void updateImage(Uri imageUri) {
-        this.imageUri = imageUri;
-        previewImage.setImageBitmap(createBitmapFromUri(imageUri));
-        clearImageButton.setEnabled((imageUri != null));
-        shareButton.setEnabled(clearTextButton.isEnabled() || clearImageButton.isEnabled());
-    }
-
-    private void updateSuffix(String suffix) {
-        SharedPreferences preferences = getSharedPreferences();
-        SharedPreferences.Editor editor = preferences.edit();
-        editor.putString(getString(R.string.key_suffix), suffix);
-        editor.apply();
-        previewSuffix.setText(suffix);
-    }
-
-    private String getSuffix() {
+    private String obtainSavedSuffix() {
         SharedPreferences preferences = getSharedPreferences();
         return preferences.getString(getString(R.string.key_suffix), "");
     }
---
+++ app/src/main/java/com/u1tramarinet/youtubemusicsharehelper/MainViewModel.java
@@ -0,0 +1,187 @@
+package com.u1tramarinet.youtubemusicsharehelper;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MediatorLiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+import java.util.Optional;
+
+public class MainViewModel extends ViewModel {
+
+    @NonNull
+    private final MutableLiveData<String> previewMusicInfoData = new MutableLiveData<>("");
+
+    @NonNull
+    private final MutableLiveData<String> previewSuffixData = new MutableLiveData<>("");
+
+    @NonNull
+    private final MediatorLiveData<String> previewTextData = new MediatorLiveData<>();
+
+    @NonNull
+    private final MutableLiveData<Uri> previewImageUriData = new MutableLiveData<>();
+
+    @NonNull
+    private final MutableLiveData<Boolean> suffixInputEnabledData = new MutableLiveData<>(false);
+
+    @NonNull
+    private final MutableLiveData<Bundle> shareEventData = new MutableLiveData<>();
+
+    @NonNull
+    private final MediatorLiveData<Boolean> clearTextButtonEnabledData = new MediatorLiveData<>();
+
+    @NonNull
+    private final MediatorLiveData<Boolean> clearImageButtonEnabledData = new MediatorLiveData<>();
+
+    @NonNull
+    private final MediatorLiveData<Boolean> shareButtonEnabledData = new MediatorLiveData<>();
+
+    public MainViewModel() {
+        previewTextData.addSource(previewMusicInfoData, (s) -> updatePreviewText());
+        previewTextData.addSource(previewSuffixData, (s) -> updatePreviewText());
+        clearTextButtonEnabledData.addSource(previewMusicInfoData, this::updateClearTextButtonEnabled);
+        clearImageButtonEnabledData.addSource(previewImageUriData, this::updateClearImageButtonEnabled);
+        shareButtonEnabledData.addSource(previewMusicInfoData, (s) -> updateShareButtonEnabled());
+        shareButtonEnabledData.addSource(previewImageUriData, (uri) -> updateShareButtonEnabled());
+    }
+
+    @NonNull
+    public MutableLiveData<String> previewSuffix() {
+        return previewSuffixData;
+    }
+
+    @NonNull
+    public LiveData<String> previewText() {
+        return previewTextData;
+    }
+
+    @NonNull
+    public LiveData<Uri> previewImageUri() {
+        return previewImageUriData;
+    }
+
+    @NonNull
+    public MutableLiveData<Boolean> suffixInputEnabled() {
+        return suffixInputEnabledData;
+    }
+
+    @NonNull
+    public LiveData<Boolean> clearTextButtonEnabled() {
+        return clearTextButtonEnabledData;
+    }
+
+    @NonNull
+    public LiveData<Boolean> clearImageButtonEnabled() {
+        return clearImageButtonEnabledData;
+    }
+
+    @NonNull
+    public LiveData<Boolean> shareButtonEnabled() {
+        return shareButtonEnabledData;
+    }
+
+    @NonNull
+    public LiveData<Bundle> shareEvent() {
+        return shareEventData;
+    }
+
+    public void updateSuffixEditing(boolean editing) {
+        Log.d(MainViewModel.class.getSimpleName(), "updateSuffixEditing()");
+        suffixInputEnabledData.postValue(editing);
+    }
+
+    public void updateSuffix(@NonNull String suffix) {
+        previewSuffixData.postValue(suffix);
+    }
+
+    public void clearText() {
+        previewMusicInfoData.postValue("");
+    }
+
+    public void clearImage() {
+        previewImageUriData.postValue(null);
+    }
+
+    public void handleIntent(@NonNull Intent intent) {
+        String action = intent.getAction();
+        String type = intent.getType();
+        Log.d(MainActivity.class.getSimpleName(), "handleIntent() action=" + action + ", type=" + type);
+
+        if (!Intent.ACTION_SEND.equals(action)) {
+            return;
+        }
+        if (type == null) {
+            return;
+        }
+
+        if ("text/plain".equals(type)) {
+            previewMusicInfoData.postValue(parseText(intent.getExtras()));
+        } else if (type.startsWith("image/")) {
+            previewImageUriData.postValue(intent.getParcelableExtra(Intent.EXTRA_STREAM));
+        }
+    }
+
+    public void shareText() {
+        String text = previewTextData.getValue();
+        Uri uri = previewImageUriData.getValue();
+        if (text == null && uri == null) return;
+
+        Bundle bundle = new Bundle();
+        if (text != null) {
+            bundle.putString(Intent.EXTRA_TEXT, text);
+        }
+        if (uri != null) {
+            bundle.putParcelable(Intent.EXTRA_STREAM, uri);
+        }
+        shareEventData.postValue(bundle);
+    }
+
+    @Nullable
+    private String parseText(@NonNull Bundle bundle) {
+        String subject = bundle.getString(Intent.EXTRA_SUBJECT, "");
+        String text = bundle.getString(Intent.EXTRA_TEXT, "");
+
+        String key = "\"";
+        int beginIndex = 1;
+        int endIndex = subject.lastIndexOf(key);
+        subject = subject.substring(beginIndex, endIndex);
+
+        if (subject.isEmpty() && text.isEmpty()) return null;
+        if (!subject.isEmpty() && !text.isEmpty()) return subject + "\n" + text;
+        return subject + text;
+    }
+
+    private void updatePreviewText() {
+        String musicInfo = Optional.ofNullable(previewMusicInfoData.getValue()).orElse("");
+        String suffix = Optional.ofNullable(previewSuffixData.getValue()).orElse("");
+        Log.d(MainViewModel.class.getSimpleName(), "updatePreviewText() musicInfo=" + musicInfo + ", suffix=" + suffix);
+        String spacer = "";
+        if (!musicInfo.isEmpty() && !suffix.isEmpty()) {
+            spacer = "\n";
+        }
+        previewTextData.postValue(musicInfo + spacer + suffix);
+    }
+
+    private void updateClearTextButtonEnabled(@Nullable String musicInfo) {
+        clearTextButtonEnabledData.postValue((!TextUtils.isEmpty(musicInfo)));
+    }
+
+    private void updateClearImageButtonEnabled(@Nullable Uri imageUri) {
+        clearImageButtonEnabledData.postValue((imageUri != null));
+    }
+
+    private void updateShareButtonEnabled() {
+        String musicInfo = previewMusicInfoData.getValue();
+        Uri imageUri = previewImageUriData.getValue();
+        boolean canShare = !TextUtils.isEmpty(musicInfo) || (imageUri != null);
+        shareButtonEnabledData.postValue(canShare);
+    }
+}
